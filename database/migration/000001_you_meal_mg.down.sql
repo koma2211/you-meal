48d6ae56f71a53f226100819fd497ef993dff595
Komron Khoshimov <komronkhoshimov@MacBook-Pro-Komron.local>
2024-09-19 12:24:11 +0500

    prepared app structere

---
+++ database/migration/000001_you_meal_mg.down.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS "self_pickups";
+
+DROP TABLE IF EXISTS "deliveries";
+
+DROP TABLE IF EXISTS "ordered_meals";
+
+DROP TABLE IF EXISTS "orders";
+
+DROP TABLE IF EXISTS "ingredients";
+
+DROP TABLE IF EXISTS "meals";
+
+DROP TABLE IF EXISTS "categories";
+
+DROP TABLE IF EXISTS "clients";
+
+DROP TABLE IF EXISTS "phones";
